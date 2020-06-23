906000e52dda82dd13d4a55fc75e94c4c504dd02
Steven Marshall <SMarshall@chef.io>
2020-06-23 12:52:02 +0100

    [phantomjs] Various refactors to existing code
    Added inspec.lock to .gitignore
    Updated README.md to latest binary format
    Added plan_name to attributes.yml
    Updates controls to add more prescriptive version checking.
    Updated inspec.yml to latest version
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/phandomjs_works.rb
+++ controls/phandomjs_works.rb
@@ -9,27 +9,32 @@ control 'core-plans-phantomjs' do
   impact 1.0
   title 'Ensure phantomjs binary is working as expected'
   desc '
-  We first check that the phantomjs binary we expect is present and then run version checks on both to verify that it is excecutable.
+  To test the phantomjs binary that core/phantomjs provides we first check for the installation directory.
+  Using this directory we then run checks to ensure the binary exists.
+  Then we test that the version of the binary we expect to be installed exists.
+    $ $PKG_PATH/bin/phantomjs --version
+      2.1.1 
   '
 
   hab_pkg_path = command("hab pkg path #{plan_ident}")
   describe hab_pkg_path do
-    its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
   end
 
   target_dir = File.join(hab_pkg_path.stdout.strip, base_dir)
 
-  phantomjs_exists = command("ls #{File.join(target_dir, "phantomjs")}")
+  phantomjs_exists = command("ls -al #{File.join(target_dir, "phantomjs")}")
   describe phantomjs_exists do
     its('stdout') { should match /phantomjs/ }
     its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
 
-  phantomjs_works = command("/bin/phantomjs -v")
+  phantomjs_works = command("#{File.join(target_dir, "phantomjs")} -v")
   describe phantomjs_works do
-    its('stdout') { should match /[0-9]+.[0-9]+.[0-9]+/ }
+    its('stdout') { should match /#{hab_pkg_path.stdout.strip.split('/')[5]}/ }
     its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
